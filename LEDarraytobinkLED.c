d1a0b106257d0c9d4bd740df51b719f1eb1e843c
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-06-20 18:18:37 +0530

    Update LEDarraytobinkLED.c

--- LEDarraytobinkLED.c
+++ LEDarraytobinkLED.c
@@ -21,3 +21,56 @@ void wait(int del)
      }
    }
 }
+
+Alternate logic 1:
+
+#include <AT89S52.h>
+void wait(int);
+void main()
+{
+   char data=0x55;
+   while(1)
+   {
+     data = ~data;/*toggles the data*/
+     P0=data;
+     wait(5); /* delay */
+   }
+}
+/* delay program */
+void wait(int del)
+{
+   int c1,c2;
+   for(c1=0;c1<del;c1++)
+   {
+     for(c2=0;c2<15;c2++)
+     {
+     }
+   }
+}
+
+Alternate logic 2:
+
+   #include <AT89S52.h>
+void wait(int);
+void main()
+{
+   char data=0xAA;
+   while(1)
+   {
+     P0=data;
+     wait(5); /* delay */
+     P0=data<<1;
+     wait(5); /* delay */
+   }
+}
+/* delay program */
+void wait(int del)
+{
+   int c1,c2;
+   for(c1=0;c1<del;c1++)
+   {
+     for(c2=0;c2<15;c2++)
+     {
+     }
+   }
+}
