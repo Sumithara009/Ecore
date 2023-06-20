84e1e577033a34b53d1e484128ba7b0ab5af32db
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-06-20 18:04:02 +0530

    Create LEDarraytobinkLED.c

---
+++ LEDarraytobinkLED.c
@@ -0,0 +1,23 @@
+#include <AT89S52.h>
+void wait(int);
+void main()
+{
+   while(1)
+   {
+     P0=0xAA;
+     wait(5); /* delay */
+     P0=0x55;
+     wait(5);  /* delay */
+   }
+}
+/* delay program */
+void wait(int del)
+{
+   int c1,c2;
+   for(c1=0;c1<del;c1++)
+   {
+     for(c2=0;c2<15;c2++)
+     {
+     }
+   }
+}
