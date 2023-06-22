a80dca598edd04ad82eb2b33057c2d87901dc487
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-06-22 18:07:41 +0530

    Update To_Blink_an_LED_Array_in_each_bit_position.c

--- To_Blink_an_LED_Array_in_each_bit_position.c
+++ To_Blink_an_LED_Array_in_each_bit_position.c
@@ -35,3 +35,32 @@ void wait(int del)
      }
    }
 }
+
+Alternate optimal code:
+
+#include <AT89S52.h>
+void wait(int);
+void main()
+{
+   char data=0x01;
+   char i;
+     while(1)
+     {
+       for(i=0;i<8;i++)/*for loop for running 8 times*/
+       {
+         P0=~(data<<i);
+         wait(1);
+       }
+     }
+}
+/* delay program */
+void wait(int del)
+{
+   int c1,c2;
+   for(c1=0;c1<del;c1++)
+   {
+     for(c2=0;c2<15;c2++)
+     {
+     }
+   }
+}
