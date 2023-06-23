5162f37cca0dc32dc1f1453c97ff54dfc7106710
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-06-23 17:57:34 +0530

    Create LED_array_to_blink_all_led.c

---
+++ LED_array_to_blink_all_led.c
@@ -0,0 +1,28 @@
+#include<AT89S52.h>
+#define SW P0
+#define LED P1
+void wait(int);
+void main()
+{
+    char s;
+    SW = 0xFF;
+    while(1)
+    {
+        s=SW;
+        LED=s;
+        wait(1);
+        LED=0xFF;
+        wait(1);
+    }
+}
+/* delay program */
+void wait(int del)
+{
+   int c1,c2;
+   for(c1=0;c1<del;c1++)
+   {
+     for(c2=0;c2<15;c2++)
+     {
+     }
+   }
+}
