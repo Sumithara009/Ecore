526e0546cb2b089079aa30c1f13bf3d703e607b7
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-07-06 11:51:20 +0530

    Update Dot_Matrix.c

--- Dot_Matrix.c
+++ Dot_Matrix.c
@@ -1,13 +1,13 @@
 #include <AT89S52.h>
 #define Row P0
 #define Col P1
-const char pattern={0x7E,0xBD,0xDB,0xE7,0xDB,0xBD,0x7E};
+const char pattern[9]={0x7E,0xBD,0xDB,0xE7,0xE7,0xDB,0xBD,0x7E};
 void main()
 {
-   /*Row=0x10;
-   Col=0xEF; for glowing of single led*/
+   Row=0x10;
+   Col=0xEF; /*for glowing of single led*/
    while(1)
-   {s
+   {
      char i;
      for(i=0;i<8;i++)
      {
